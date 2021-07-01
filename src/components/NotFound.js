09858d35613dd0a6cf812ebcd63ca56e0285e0cb
ArzuAshik <arzu2050@gmail.com>
2021-07-01 20:32:58 +0600

    auto redirect to home

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 const NotFound = () => {
+    const timerRef = useRef(null);
+    const history = useHistory();
+    const [remainingTime, setRemainingTime] = useState(5);
+    useEffect(() => {
+        timerRef.current = setInterval(() => {
+            setRemainingTime(currentState => currentState - 1);
+        }, 1000);
+    }, []);
+    
+    if(remainingTime < 0){
+        clearInterval(timerRef.current);
+        history.push("/");
+    }
     return (
         <div className="not-found">
             <h1>404</h1>
-            <h1>Page Not Found!</h1>
+            <h2>Page Not Found!</h2>
+            <p>Redirecting to The Home Page in <span>{remainingTime}</span> Seconds.</p>
         </div>
     );
 };
